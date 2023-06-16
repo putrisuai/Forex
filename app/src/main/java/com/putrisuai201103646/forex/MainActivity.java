00ad26b65e7601a5b15f736b90f7cfd698efc659
cyrilla <127930707+putrisuai@users.noreply.github.com>
2023-06-16 14:58:54 +0700

    putri

--- app/src/main/java/com/putrisuai201103646/forex/MainActivity.java
+++ app/src/main/java/com/putrisuai201103646/forex/MainActivity.java
@@ -18,6 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         swipeRefreshLayout1 = (SwipeRefreshLayout)findViewById(R.id.swipeRefreshLayout1);
-        idrTextView = (TextView)
+        idrTextView = (TextView)findViewById(R.id.idrTextView);
+        usdTextView = (TextView)findViewById(R.id.usdTextView);
+        aedTextView = (TextView)findViewById(R.id.aedTextView);
+        afnTextView = (TextView)findViewById(R.id.afnTextView);
+        allTextView = (TextView)findViewById(R.id.allTextView);
+        amdTextView = (TextView)findViewById(R.id.amdTextView);
+        angTextView = (TextView)findViewById(R.id.angTextView);
+        aoaTextView = (TextView)findViewById(R.id.aoaTextView);
+        arsTextView = (TextView)findViewById(R.id.arsTextView);
+        audTextView = (TextView)findViewById(R.id.audTextView);
+        loadingProgressBar = (ProgressBar)findViewById(R.id.loadingProgressBar);
+
+        initSwipeRefreshLayout();
+        initForex();
+    }
+
+    private void initSwipeRefreshLayout() {
+        swipeRefreshLayout1.setRefreshing(()  {
+            initForex();
+
+            swipeRefreshLayout1.setRefreshing(false);
+        });
     }
 }
